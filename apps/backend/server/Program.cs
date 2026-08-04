1451bfba052bdf3c7919b302cc7da9d5068a2ccd
arcwagner1337 <wellernam1788@gmail.com>
2026-08-04 22:44:40 +0700

    beginnig c# server and db init

---
+++ apps/backend/server/Program.cs
@@ -0,0 +1,40 @@
+using Npgsql;
+using System.Data;
+using server.Data;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+DatabaseInitializer.Initialize(connectionString);
+
+// 2. Регистрация IDbConnection для Dapper
+builder.Services.AddScoped<IDbConnection>(_ => new NpgsqlConnection(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
